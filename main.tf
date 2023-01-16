1a22dc79631cae5c1086ecbbc024d37208ba0439
Eric Vallee <eric_vallee@webificservices.com>
2023-01-16 13:03:19 -0500

    feat: INFRA-225 Added suport for TXT records

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ locals {
           cache_ttl = var.cache_ttl
           a_records = var.a_records
           mx_records = var.mx_records
+          txt_records = var.txt_records
           dns_server_name = local.dns_server_name
           email = local.email
           dns_server_ips = var.dns_server_ips
@@ -39,6 +40,7 @@ resource "openstack_objectstorage_object_v1" "zonefile" {
       cache_ttl = var.cache_ttl
       a_records = var.a_records
       mx_records = var.mx_records
+      txt_records = var.txt_records
       dns_server_name = local.dns_server_name
       email = local.email
       dns_server_ips = var.dns_server_ips
